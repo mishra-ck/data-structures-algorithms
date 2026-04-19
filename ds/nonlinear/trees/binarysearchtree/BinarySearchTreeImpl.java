73fe7bd1e2fd967b90e66ac1c8a6ca84cb66ad83
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-19 17:59:47 +0530

    Added Inorder traversal

---
+++ ds/nonlinear/trees/binarysearchtree/BinarySearchTreeImpl.java
@@ -0,0 +1,5 @@
+package ds.nonlinear.trees.binarysearchtree;
+
+public class BinarySearchTreeImpl {
+
+}
--- ds/nonlinear/trees/binarytree/BinaryTreeImpl.java
+++ ds/nonlinear/trees/binarytree/BinaryTreeImpl.java
@@ -1,5 +1,9 @@
 package ds.nonlinear.trees.binarytree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BinaryTreeImpl<T> implements Comparable<T>{
     protected BinaryTreeNode<T> root;
     protected int size;
@@ -18,7 +22,45 @@ public class BinaryTreeImpl<T> implements Comparable<T>{
     /**  check if tree is empty  */
     public boolean isEmpty(){ return root == null;}
 
+    /**  Tree Traversals( Inorder/Preorder/Postorder )  */
     /**  Inorder Traversal : Left -> Root -> Right - gives sorted output */
+    public List<T> inorder(){
+        List<T> result = new ArrayList<>();
+        inorderRecursiveHelper(root,result);  // recursive way
+        inorderIterativeHelper(root);  // iterative way
+        return result;
+    }
+    // Recursive approach
+    private void inorderRecursiveHelper(BinaryTreeNode<T> node, List<T> result) {
+        if(node == null) return;
+        inorderRecursiveHelper(node.getLeft(),result);
+        result.add(node.getValue()); // add current node value in list
+        inorderRecursiveHelper(node.getRight(),result);
+    }
+    // Iterative approach
+    private void inorderIterativeHelper(BinaryTreeNode<T> node){
+
+        if(node == null) return;
+
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        BinaryTreeNode<T> current = node;
+
+        while(current != null && (!stack.isEmpty())){
+
+            // 1. Go to left most of the current node
+            while(current != null ){
+                stack.push(current);
+                current = current.getLeft();
+            }
+            // 2. current will null by now, pop the item
+            current = stack.pop();
+            System.out.println("Visited node : "+ current.getValue());
+
+            // 3. Visited left now go right
+            current = current.getRight();
+
+        }
+    }
 
     @Override
     public int compareTo(T o) {
